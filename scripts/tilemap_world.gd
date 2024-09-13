767218bb46ff9f738ec451040a1cb7fb2c3836f0
Seton Webber <setonwebber@gmail.com>
2024-09-13 22:19:09 +0800

    finished world tilemap generation and basic placement functions

---
+++ scripts/tilemap_world.gd
@@ -0,0 +1,54 @@
+extends TileMap
+
+
+var chunkSize = 16
+var mapSize = 16
+var dic = {}
+
+var rng = RandomNumberGenerator.new()
+
+func loadChunk(mapX, mapY):
+	for x in chunkSize:
+		for y in chunkSize:
+			rng.randomize()
+			match rng.randi_range(0, 4):
+				0:
+					dic[str(Vector2(x + mapX,y + mapY))] = {
+						"Type": "Heavy Grass"
+						}
+					set_cell(0, Vector2(x + mapX,y + mapY), 1, Vector2i(0,0), 0)
+				1:
+					dic[str(Vector2(x + mapX,y + mapY))] = {
+						"Type": "Light Grass"
+						}
+					set_cell(0, Vector2(x + mapX,y + mapY), 1, Vector2i(1,0), 0)
+				2:
+					dic[str(Vector2(x + mapX,y + mapY))] = {
+						"Type": "Big Bushes"
+						}
+					set_cell(0, Vector2(x + mapX,y + mapY), 1, Vector2i(2,0), 0)
+				3:
+					dic[str(Vector2(x + mapX,y + mapY))] = {
+						"Type": "Small Bushes"
+						}
+					set_cell(0, Vector2(x + mapX,y + mapY), 1, Vector2i(3,0), 0)
+				4:
+					dic[str(Vector2(x + mapX,y + mapY))] = {
+						"Type": "Black"
+						}
+					set_cell(0, Vector2(x + mapX,y + mapY), 1, Vector2i(4,0), 0)
+
+func _ready():
+	for x in mapSize * 2:
+		if x > mapSize:
+			x = (x * -1) % mapSize 
+		for y in mapSize * 2:
+			if y > mapSize:
+				y =  (y * -1) % mapSize
+			loadChunk(chunkSize	* x, chunkSize	* y)
+
+func _process(delta):
+	var tile = local_to_map(get_global_mouse_position())
+	
+	if Input.is_action_pressed("left_click"):
+		set_cell(2, tile, 2, Vector2i(0,0), 0)
