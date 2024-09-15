7756ac357429fba9e0c36c9f9a49bed9930c4b15
setonwebber <setonwebber@gmail.com>
2024-09-15 13:16:29 +0800

    started work on placing buildings with a bigger size that 1x1

--- scripts/tilemap_buildings.gd
+++ scripts/tilemap_buildings.gd
@@ -5,20 +5,22 @@ var occupiedTiles = []
 var buildingType
 var scene_to_instance = preload("res://entities/miner.tscn")
 var buildingNumber = 0
+var buildingSize
+var minerSize = 3
 
 func _process(_delta: float) -> void:
 	var tile = local_to_map(get_global_mouse_position())
-	if Input.is_action_just_released("left_click"):
-		# get whatever building is in the hotbar
-		buildingType = "miner"
-		# get size of building 
+	if Input.is_action_just_released("left_click") and occupiedTiles.has(tile):
+		for x in minerSize:
+			tile 
+			for y in minerSize:
+				occupiedTiles.append(tile + x)
 		
-		if !occupiedTiles.has(tile):
-			occupiedTiles.append(tile)
-			instance_object(tile)
+		occupiedTiles.append(tile)
+		placeBuilding(tile)
 
 
-func instance_object(position):
+func placeBuilding(position):
 	buildingNumber =+ 1
 	var object = scene_to_instance.instantiate()
 	object.position = position * 64
