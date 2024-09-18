2bcde7d6bda5350974b96e9e419c952a7f89559f
Seton Webber <setonwebber@gmail.com>
2024-09-18 09:15:59 +0800

    started adding items

--- scripts/tilemap_buildings.gd
+++ scripts/tilemap_buildings.gd
@@ -26,17 +26,22 @@ func _ready() -> void:
 			hotbarObjects.append("null")
 
 func _process(_delta: float) -> void:
-	getBuildingDetails()
+	getBuildingDetails(hotbarObjects[Global.HUD_HOTBAR_CURRENTID].instantiate())
 	var tile = Vector2(local_to_map(get_global_mouse_position()))
+	placedBuilding.emit(occupiedTiles)
 	
 	if Input.is_action_pressed("left_click") and !Global.HUD_SELECTION_OVERBUILDING and !Functions.checkIfTilesOccupied(tile, buildingSize, occupiedTiles)[0]:
+		object = hotbarObjects[Global.HUD_HOTBAR_CURRENTID].instantiate()
 		placeBuilding(tile)
-		placedBuilding.emit(occupiedTiles)
+		
 		for tileBuf in Functions.checkIfTilesOccupied(tile, buildingSize, occupiedTiles)[1]:
+			print(str("append", tileBuf))
 			occupiedTiles.append(tileBuf)
+		print(occupiedTiles)
 	
 	if Input.is_action_pressed("right_click") and Global.HUD_SELECTION_OVERBUILDING and is_instance_valid(Global.HUD_SELECTION_BUILDING):
 		Global.HUD_SELECTION_BUILDING.queue_free()
+		getBuildingDetails(Global.HUD_SELECTION_BUILDING) # change to building placed
 		var pos
 		if buildingSize % 2 == 0:
 			pos = (Global.HUD_SELECTION_BUILDING.position - Vector2(gridSize, gridSize)) / gridSize 
@@ -44,16 +49,17 @@ func _process(_delta: float) -> void:
 			pos = (Global.HUD_SELECTION_BUILDING.position - Vector2(gridSize / 2, gridSize / 2)) / gridSize 
 		
 		for tileBuf in Functions.checkIfTilesOccupied(pos, buildingSize, occupiedTiles)[1]:
+			print(str("erase", tileBuf))
 			occupiedTiles.erase(tileBuf)
 		Global.HUD_SELECTION_OVERBUILDING = false
+		print(occupiedTiles)
 
 func placeBuilding(pos):
-	buildingNumber += 1
 	if buildingSize % 2 == 0:
 		object.position = pos * gridSize + Vector2(gridSize, gridSize)
 	else:
 		object.position = pos * gridSize + Vector2(gridSize / 2, gridSize / 2)
-	object.name = str(buildingType, buildingNumber)
+	object.name = str(buildingType, "_at", pos, "_facing", Global.HUD_SELECTION_ROTATION)
 	updateBuildingDetails()
 	match Global.HUD_SELECTION_ROTATION:
 		"south":
@@ -79,9 +85,8 @@ func updateBuildingDetails():
 	buildingComponent.buildingSize = buildingSize
 	buildingComponent.buildingTier = buildingTier
 
-func getBuildingDetails():
-	object = hotbarObjects[Global.HUD_HOTBAR_CURRENTID].instantiate()
-	var buildingComponent = object.get_child(0)
+func getBuildingDetails(obj):
+	var buildingComponent = obj.get_child(0)
 	buildingType = buildingComponent.buildingType
 	buildingSize = buildingComponent.buildingSize
 	buildingTier = buildingComponent.buildingTier
